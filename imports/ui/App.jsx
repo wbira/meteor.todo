a96cfb89445074ce528273ea6f760a86e96b8285
Waldemar Bira <waldek.bira@gmail.com>
2016-11-11 17:26:13 +0100

    Added filtering completed tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,14 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx'
 
 class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            hideCompleted: false
+        }
+    }
+
     handleSubmit(event) {
         event.preventDefault();
 
@@ -19,8 +27,17 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({ hideCompleted: !this.state.hideCompleted })
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (<Task key={task._id} task={task} />));
+        let filteredTasks = this.props.tasks;
+
+        if(this.state.hideCompleted){
+            filteredTasks = filteredTasks.filter((task) => !task.checked)
+        }
+        return filteredTasks.map((task) => (<Task key={task._id} task={task} />));
     }
 
     render() {
@@ -28,6 +45,15 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+
+                    <label className="hide-completed">
+                        <input type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)} />
+                        Hide completed tasks.
+                    </label>
+
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
                         <input type="text" ref="textInput" placeholder="Type to add new tasks" />
                     </form>
@@ -47,6 +73,6 @@ App.propTypes = {
 
 export default createContainer(() => {
     return {
-        tasks: Tasks.find({}, { sort: { createdAt: -1 }}).fetch(),
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     };
 }, App);
