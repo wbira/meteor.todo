a6b678d5c69455224a36e922aeb2bc39ad977a76
Waldemar Bira <waldek.bira@gmail.com>
2016-11-11 15:05:05 +0100

    Implemented methods for delete and toggle tasks

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,6 +1,18 @@
 import React, { Component, PropTypes } from 'react';
+import Tasks from '../api/tasks.js';
+
 
 export default class Task extends Component {
+    toggleChecked() {
+        Tasks.update(this.props.task._id, {
+            $set: { checked: !this.props.task.checked },
+        });
+    }
+
+    deleteTask() {
+        Tasks.remove(this.props.tasks._id);
+    }
+
     render() {
         return (<li>{this.props.task.text}</li>);
     }
