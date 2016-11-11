95112f4db9bb0a309a5cc7ccf050c3960605d9e4
Waldemar Bira <waldek.bira@gmail.com>
2016-11-11 01:27:18 +0100

    Implemented form and handler for adding new tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,22 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx'
 
 class App extends Component {
-    getTasks() {
-        return [
-            { _id: 1, text: 'This is task 1' },
-            { _id: 2, text: 'This is task 2' },
-            { _id: 3, text: 'This is task 3' }
-        ];
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({
+            text,
+            createdAt: new Date()
+        })
+
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
     renderTasks() {
@@ -22,6 +28,9 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+                        <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+                    </form>
                 </header>
 
                 <ul>
@@ -38,6 +47,6 @@ App.propTypes = {
 
 export default createContainer(() => {
     return {
-        tasks: Tasks.find({}).fetch(),
+        tasks: Tasks.find({}, { sort: { createdAt: -1 }}).fetch(),
     };
 }, App);
